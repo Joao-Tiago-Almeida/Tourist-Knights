e883dd56ca26c6ab00faf8cd1f07169ac1b53b28
Miguel Fazenda <miguelsfazenda@gmail.com>
2018-12-08 19:46:06 +0000

    limpeza do dijkstra

--- src/movimentos.h
+++ src/movimentos.h
@@ -11,11 +11,11 @@
 bool inside_board(Tabuleiro *tabuleiro, Vector2 vec);
 bool city_valid(Tabuleiro *tabuleiro, Vector2 vec);
 bool check_if_city_accessible(Tabuleiro* tabuleiro, Vector2 c);
+bool do_points_make_L(Vector2 point1, Vector2 point2);
 
 Path inverte_caminho(Tabuleiro* tabuleiro, Path path);
 
-Path* dijkstra2(Tabuleiro* tabuleiro, Vector2 ini, Vector2* destinos, int num_destinos);
-unsigned short movimentos_find_better_path_cost(Tabuleiro* tabuleiro, Vector2 ini, Vector2 dest); //TODO tirar
-Path movimentos_find_path(Tabuleiro* tabuleiro, Vector2 ini, Vector2 dest);
+Path* movimentos_find_path_to_multiple_cities(Tabuleiro* tabuleiro, Vector2 ini, Vector2* destinos, int num_destinos);
+Path movimentos_find_path_to_city(Tabuleiro* tabuleiro, Vector2 ini, Vector2 dest);
 
 #endif
